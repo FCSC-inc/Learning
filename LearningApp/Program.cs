44d6bdbd34d786cac7f3dea1ae4f6be0ec8237e7
Bryce Waldschmidt <bryce@waldschmidt.com>
2021-11-17 22:09:15 -0500

    GODS WILL BE DONE

--- LearningApp/Program.cs
+++ LearningApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System; //
+﻿using System; 
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
 namespace LearningApp
 {
 
@@ -6,24 +10,49 @@ namespace LearningApp
 
     class Program
     {
-        static void main()
-        {
-        }
+      
         static void Main(string[] args)
         {
 
             Console.BackgroundColor = ConsoleColor.Magenta;
             Console.ForegroundColor = ConsoleColor.Black;
-            
 
-            string cuck = "Fuck dees hoes";
-            
+            List<int> Fuckoff = new List<int>();
+            Fuckoff.Add(1);
+            Fuckoff.Add(2);
+            Fuckoff.Add(3);
+            Fuckoff.Add(4);
 
-            
+            var cities = new List<string>();
+            cities.Add("New Bloxburg");
+            cities.Add("Lubba's Cave");
+            cities.Add("Argentina (The Country)");
+            cities.Add("FUck Off Ville");
 
+            var bigCities = new List<string>()
+            {
+                "New Bloxburg",
+                "Lubba's Cave",
+                "Argentina (The Country)",
+                "Fuck off Ville",
+            };
 
-            Console.Write(cuck);
+            string[] Hitlerdidnothingwrong = new string[3] { "lubba's House", "Lubba's Bathroom", "Lubba's THoughts" };
+
+            var popularCities = new List<string>();
+
+            popularCities.AddRange(cities);
+            var favouriteCities = new List<string>();
+
+            Console.WriteLine(Hitlerdidnothingwrong[0]);
+            Console.WriteLine(Hitlerdidnothingwrong[1]);
+            Console.WriteLine(Hitlerdidnothingwrong[2]);
             Console.ReadLine();
+
+
+
+
+            
 
 
 
