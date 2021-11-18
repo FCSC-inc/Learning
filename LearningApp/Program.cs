52270a82f617bb1aee00ae2e44129ee006df169c
Bryce Waldschmidt <bryce@waldschmidt.com>
2021-11-17 20:54:44 -0500

    fuck hoes

--- LearningApp/Program.cs
+++ LearningApp/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace LearningApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("dumb ass shit");
+            Console.ReadLine();
+
+
+
+
+
         }
+
+
     }
 }
